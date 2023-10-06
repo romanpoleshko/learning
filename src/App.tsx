82944e21a59edf50df571aa4ea812ba19f4e2cab
Roman Poleshko <romanpoleshko@MacBook-Pro-Roman.local>
2023-10-06 13:23:10 +0200

    feat: add user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { GoogleLogin } from '@react-oauth/google';
+import { useState } from 'react';
+import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 
-import './App.css';
+const App = () => {
+  const [ userData, setUserData ] = useState<Record<string, string> | null>(null);
 
-function App() {
+  const onSuccess = (credentialResponse: CredentialResponse) => {
+    const decoded: Record<string, string> = jwt_decode(credentialResponse.credential || '');
 
-  return (
-    <GoogleLogin
-      onSuccess={credentialResponse => {
-        console.log(credentialResponse);
-      }}
-      onError={() => {
-        console.log('Login Failed');
-      }}
-    />
-  );
-}
+    if (decoded) setUserData(decoded);
+  };
+
+  const onError = () => {
+    console.log('Login Failed');
+  }
+
+  return userData
+    ? (
+      <form>
+        <img src={userData.picture} alt="user_picture" />
+        <p>Email: {userData.email}</p>
+        <p>Name: {userData.name}</p>
+      </form>
+    )
+    : <GoogleLogin onSuccess={onSuccess} onError={onError} />;
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import App from './App.tsx';
 import './index.css';
 
